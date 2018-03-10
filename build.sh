25f48b0673718d0c74d0c40654169057e140b641
Harry Boyes <harry.boyes@myunidays.com>
2018-03-10 17:12:08 +0000

    Add offers and OS specific zipping to build script

--- build.sh
+++ build.sh
@@ -2,9 +2,18 @@
 
 set -e
 
-cd endpoints/me
+declare -a endpoints=("me" "offers")
 
-GOOS=linux go build -o main
-# zip deployment.zip main
+for i in "${endpoints[@]}"
+do
+	cd endpoints/$i
+	GOOS=linux go build -o main
 
-cd -
+	if [[ $(uname) == "MINGW"* ]]; then
+		~/go/bin/build-lambda-zip.exe -o deployment.zip main
+	else
+		zip deployment.zip main
+	fi
+
+	cd -
+done
