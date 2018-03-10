432f571a60ea0a0c1faa1222efa0bff03b56d476
Alex Young <alex@heuris.io>
2018-03-10 13:34:46 +0000

    works now you know it

--- endpoints/me/main.go
+++ endpoints/me/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest() {
-	// return "hello", nil
+func handleRequest(context context.Context,
+	request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	return events.APIGatewayProxyResponse{
+		Body:       string("{}"),
+		StatusCode: 200,
+	}, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(handleRequest)
 }
