cc82dd5f777342c886be99496aaddc660694b55f
Harry Boyes <harry.boyes@myunidays.com>
2018-03-10 15:16:47 +0000

    Make Offer.Reservation a pointer

--- models/offer.go
+++ models/offer.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Offer struct {
-	Id          uuid.UUID   `json:"id"`
-	Name        string      `json:"name"`
-	CreatedOn   time.Time   `json:"createdOn"`
-	CreatedBy   uuid.UUID   `json:"createdBy"`
-	Location    Location    `json:"location"`
-	Reservation Reservation `json:"reservation"`
+	Id          uuid.UUID    `json:"id"`
+	Name        string       `json:"name"`
+	CreatedOn   time.Time    `json:"createdOn"`
+	CreatedBy   uuid.UUID    `json:"createdBy"`
+	Location    Location     `json:"location"`
+	Reservation *Reservation `json:"reservation"`
 }
 
 type Location struct {
