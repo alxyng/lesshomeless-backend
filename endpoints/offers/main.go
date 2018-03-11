7a0b261a31a26650b6c24c0c3f234e6182c0270a
Harry Boyes <harry.boyes@myunidays.com>
2018-03-11 11:15:34 +0000

    Get distances on /offers GET

--- endpoints/offers/main.go
+++ endpoints/offers/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -32,7 +36,7 @@ func handleRequest(context context.Context,
 		return post(request, userService, offerService)
 	}
 
-	return get(offerService)
+	return get(offerService, request.QueryStringParameters["lat"], request.QueryStringParameters["long"])
 }
 
 func post(request events.APIGatewayProxyRequest, userSvc user.UserService, svc offer.OfferService) (events.APIGatewayProxyResponse, error) {
@@ -84,14 +88,48 @@ func post(request events.APIGatewayProxyRequest, userSvc user.UserService, svc o
 	}, nil
 }
 
-func get(svc offer.OfferService) (events.APIGatewayProxyResponse, error) {
+func get(svc offer.OfferService, lat string, long string) (events.APIGatewayProxyResponse, error) {
 	offers, err := svc.GetAllOffers()
 	if err != nil {
 		log.Printf("error getting all offers: %v\n", err)
 		return helpers.CreateInternalServerErrorResponse()
 	}
 
-	data, err := json.Marshal(offers)
+	var offersWithDistance []OfferWithDistance
+
+	for _, o := range offers {
+		var netTransport = &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: 10 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 10 * time.Second,
+		}
+		var netClient = &http.Client{
+			Timeout:   time.Second * 10,
+			Transport: netTransport,
+		}
+		url := "http://dev.virtualearth.net/REST/v1/Routes/Walking?wayPoint.1=" + lat + "," + long + "&wayPoint.2=" + fmt.Sprint(o.Location.Lat) + "," + fmt.Sprint(o.Location.Long) + "&key=AheZ4uLLEL_fjKgtvICY0JBnbwYgJMxQe-u_54Pht6VEn1ZiI684arsTbjTR3t9n"
+		fmt.Printf("Yooo - %v\n", url)
+		resp, err := netClient.Get(url)
+		if err != nil {
+			log.Printf("error getting maps: %v\n", err)
+			return helpers.CreateInternalServerErrorResponse()
+		}
+
+		var travelData BingMapsResponse
+		if err := json.NewDecoder(resp.Body).Decode(&travelData); err != nil {
+			log.Printf("error decoding distance body: %v\n", err)
+			return helpers.CreateInternalServerErrorResponse()
+		}
+		fmt.Printf("Hullo - %v\n", travelData)
+		offersWithDistance = append(offersWithDistance, OfferWithDistance{
+			Offer:    o,
+			Distance: travelData.ResourceSets[0].Resources[0].TravelDistance,
+		})
+		fmt.Printf("Dope! - %v\n", offersWithDistance)
+	}
+	fmt.Printf("Holla dolla")
+	data, err := json.Marshal(offersWithDistance)
 	if err != nil {
 		log.Printf("error marshalling offers: %v\n", err)
 		return helpers.CreateInternalServerErrorResponse()
@@ -105,4 +143,21 @@ func get(svc offer.OfferService) (events.APIGatewayProxyResponse, error) {
 
 func main() {
 	lambda.Start(handleRequest)
+}
+
+type OfferWithDistance struct {
+	Offer    models.Offer
+	Distance float32
+}
+
+type BingMapsResponse struct {
+	ResourceSets []ResourceSet `json:"resourceSets"`
+}
+
+type ResourceSet struct {
+	Resources []Resource `json:"resources"`
+}
+
+type Resource struct {
+	TravelDistance float32 `json:"travelDistance"`
 }
